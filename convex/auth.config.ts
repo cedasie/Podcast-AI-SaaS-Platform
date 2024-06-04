e581ab8c8c8235808e9154ab5fce153b714e8aeb
cedasie <conquete.asie@gmail.com>
2024-06-04 23:48:53 +0200

    add convex and clerk and connect both of them together

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://evident-haddock-88.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
